cf6cf2b012f161549b36a7d3582df441569b9234
Fabio <fabius.scal@libero.it>
2021-02-01 18:02:57 +0100

    WEF? ero proprio un single immaturo. grazie Fra <3

--- TAP1718_Laboratorio3_Git/Program.cs
+++ TAP1718_Laboratorio3_Git/Program.cs
@@ -7,6 +7,9 @@ namespace TAP1718_Laboratorio3_Git
         public static void Main(string[] args)
         {
             Console.WriteLine("invecE CAPOSVCI TUTTO");
+
+            Console.WriteLine("Esatto, te lo dice il fabio del futuro, del primo febbraio, che ha " +
+                              "3 esami in meno da dare lel");
         }
     }
 }
